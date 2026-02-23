d5fd468ed66933c2a8860e958676812338f5bb7c
Aitor Iturrioz Rodríguez <aiturrioz@tknika.eus>
2026-02-23 13:34:17 +0100

    Improve srsLTE image
    - Embed the configuration folder into the container (can be overwritten)
    - Add the option to configure the eNB ID through an env var
    
    Signed-off-by: Aitor Iturrioz Rodríguez <aiturrioz@tknika.eus>

--- srslte/srslte_init.sh
+++ srslte/srslte_init.sh
@@ -65,8 +65,10 @@ else
 	echo "Error: Invalid component name: '$COMPONENT_NAME'"
 fi
 
-sed -i 's|MNC|'$MNC'|g' /etc/srsran/enb.conf
-sed -i 's|MCC|'$MCC'|g' /etc/srsran/enb.conf
+sed -i 's|ENB_ID|'${ENB_ID:-0x19B}'|g' /etc/srsran/enb.conf
+sed -i 's|MNC|'${MNC:-01}'|g' /etc/srsran/enb.conf
+sed -i 's|MCC|'${MCC:-001}'|g' /etc/srsran/enb.conf
+sed -i 's|TAC|'${TAC:-1}'|g' /etc/srsran/rr.conf
 sed -i 's|SRS_ENB_IP|'$SRS_ENB_IP'|g' /etc/srsran/enb.conf
 sed -i 's|SRS_UE_IP|'$SRS_UE_IP'|g' /etc/srsran/enb.conf
 sed -i 's|UE1_KI|'$UE1_KI'|g' /etc/srsran/ue.conf
@@ -75,7 +77,6 @@ sed -i 's|UE1_IMSI|'$UE1_IMSI'|g' /etc/srsran/ue.conf
 sed -i 's|SRS_UE_IP|'$SRS_UE_IP'|g' /etc/srsran/ue.conf
 sed -i 's|SRS_ENB_IP|'$SRS_ENB_IP'|g' /etc/srsran/ue.conf
 sed -i 's|SRS_GNB_IP|'$SRS_GNB_IP'|g' /etc/srsran/ue.conf
-sed -i 's|TAC|'$TAC'|g' /etc/srsran/rr.conf
 
 # For dbus not started issue when host machine is running Ubuntu 22.04
 service dbus start && service avahi-daemon start
